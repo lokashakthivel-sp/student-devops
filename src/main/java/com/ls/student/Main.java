06509fd16125bb4f559dc1ca07a0ec4e6fcacdc7
LokashakthivelSP <lokashakthivelsp@gmail.com>
2026-08-12 14:33:18 +0530

    second

--- src/main/java/com/ls/student/Main.java
+++ src/main/java/com/ls/student/Main.java
@@ -1,4 +1,18 @@
 package com.ls.student;
-
+import java.nio.file.Path;
+import java.nio.file.Paths;
 public class Main {
+    public static void main(String[] args) {
+        if (args.length < 1) {
+            System.out.println("Usage: Mp3Util <path-to-mp3-file>");
+            return;
+        }
+        Path mp3Path = Paths.get(args[0]);
+        String artist = Sub.getArtist(mp3Path);
+        if (artist != null) {
+            System.out.println("Artist: " + artist);
+        } else {
+            System.out.println("Could not read artist from: " + mp3Path);
+        }
+    }
 }
--- src/main/java/com/ls/student/Sub.java
+++ src/main/java/com/ls/student/Sub.java
@@ -1,4 +1,19 @@
 package com.ls.student;
-
+import com.mpatric.mp3agic.*;
+import java.io.IOException;
+import java.nio.file.Path;
 public class Sub {
+    public static String getArtist(Path path) {
+        try {
+            Mp3File mp3file = new Mp3File(path);
+            ID3v2 id3v2Tag = mp3file.getId3v2Tag();
+            if (id3v2Tag != null) {
+                String albumArtist = id3v2Tag.getAlbumArtist();
+                return albumArtist != null ? albumArtist : id3v2Tag.getArtist();
+            }
+            return null;
+        } catch (IOException | UnsupportedTagException | InvalidDataException e) {
+            return null;
+        }
+    }
 }
